6553fac251a51cf910f495320c0baf8c0cc504e9
Jill Lynch <jill.lynch5@gmail.com>
2014-07-20 18:13:48 -0500

    only owner can delete restaurant

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,3 +1,16 @@
-def show
-  #@owner = Owner.find(params[:id])
+class OwnersController < ApplicationController
+	before_filter :authenticate_owner!
+
+  def index
+    @owners = Owner.all
+  end
+
+  def show
+    @owner = Owner.find(params[:id])
+    unless @owner == current_owner
+      redirect_to :back, :alert => "Access denied."
+    end
+  end
+
 end
+
